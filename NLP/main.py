42fac9a159d5d0f997cdb13f200e56625ace7b96
Unknown <y.creak@hotmail.com>
2021-04-26 10:54:31 +0200

    NLP4
    Now output to dataframe and CSV

--- NLP/main.py
+++ NLP/main.py
@@ -1,6 +1,10 @@
 from pprint import pprint
 from Questgen import main
 import pickle
+import pandas as pd
+
+column_names = ["question", "answer"]
+df = pd.DataFrame(columns = column_names)
 
 # f = open("payload.txt")
 # line = f.read().replace("\n", " ")
@@ -13,12 +17,15 @@ import pickle
 import wikipedia    # Specify the title of the Wikipedia page
 # wiki = wikipedia.page('Nazi Germany') # Extract the plain text content of the page
 # text = wiki.content
-subjects = ['Barrack Obama', 'Netherlands','Dinosaur']
+
+with open('subjects.txt') as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+subjects = [x.strip() for x in content] 
 
 loading = False
 
-
-
+myDict = {}
 
 for subject in subjects:
 
@@ -50,5 +57,10 @@ for subject in subjects:
     for item in output['questions']:
         # print(item)
         print(item['Question'], item['Answer'])
+        new_line = {'question': item['Question'], 'answer': item['Answer']}
+        df = df.append(new_line, ignore_index=True)
+
 
 print('end')
+print(df)
+df.to_csv('./dataframe.csv', index = False, header=True)
